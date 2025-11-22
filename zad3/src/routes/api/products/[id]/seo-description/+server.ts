498576b544ab022f8be07bbb596d5e51171d7de1
mxkcpy <mpawlos20@gmail.com>
2025-11-22 14:50:39 +0100

    before mod

--- zad3/src/routes/api/products/[id]/seo-description/+server.ts
+++ zad3/src/routes/api/products/[id]/seo-description/+server.ts
@@ -1,15 +1,19 @@
 import { json } from '@sveltejs/kit';
-import { ProductModel } from '$lib/model/Product';
+import { ProductModel, type Product } from '$lib/model/Product';
 import { createDeleteHandler } from '$lib/api/httpMethods';
 import type { RequestHandler } from './$types';
 import { StatusCodes } from 'http-status-codes';
+import Groq from "groq-sdk";
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 export const GET: RequestHandler = async ({ params }) => {
   try {
     const { id } = params
     const product = await ProductModel.findById(id);
+    const description = await getGroqProductDescription(product);
 
-    return json(product, { status: StatusCodes.OK });
+    return json(description, { status: StatusCodes.OK });
 
   } catch (error) {
     console.error("Error fetching:", error);
@@ -17,3 +21,21 @@ export const GET: RequestHandler = async ({ params }) => {
     return json({ error: 'Could not fetch' }, { status: StatusCodes.INTERNAL_SERVER_ERROR });
   }
 };
+
+async function getGroqProductDescription(product: any) {
+
+  return groq.chat.completions.create({
+
+    messages: [
+      {
+        role: "user",
+
+        content: "Generate a 3 sentence description, that will make user anxious to buy this product: {product}",
+
+      },
+
+    ],
+
+    model: "openai/gpt-oss-20b",
+  });
+}
